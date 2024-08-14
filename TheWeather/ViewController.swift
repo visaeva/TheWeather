c43e9ae20606c705d275496148b99ad6a8129665
visaeva <visaeva0302@gmail.com>
2024-08-14 14:32:02 +0300

    Initial Commit

---
+++ TheWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheWeather
+//
+//  Created by Victoria Isaeva on 14.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
